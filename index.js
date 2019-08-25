c9cf0f3c8d3f5750b949884a2c687c9c36378a8d
Andrey Malinin <isaac220515@gmail.com>
2019-08-25 13:45:26 +0300

    релиз

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var h = 3;
 var w = 7;
 
 // размер блоков
-var h_block = 127;
-var w_block = 93;
+var h_el = 127;
+var w_el = 93;
 
 //создание 2мер массива для сохранения информации о подкрепленных элементах
 var info_about_el_in_block = [];
@@ -53,8 +53,8 @@ for (var i = 0; i < h; i++) {
     for (var k = 0; k < w; k++) {
         counter++;
         var d = document.createElement('div');
-        d.style.width = w_block + 'px';
-        d.style.height = h_block + 'px';
+        d.style.width = w_el + 'px';
+        d.style.height = h_el + 'px';
         d.style.cursor = "move";
         d.style.position = "absolute";
         d.style.backgroundImage = "url(images/image_part_" + pad(String(counter), 3) + ".jpg)";
@@ -68,8 +68,7 @@ for (var i = 0; i < h; i++) {
             var thisBlock = this;
             let shiftX = event.clientX - thisBlock.getBoundingClientRect().left;
             let shiftY = event.clientY - thisBlock.getBoundingClientRect().top;
-
-            thisBlock.style.position = 'absolute';
+            
             thisBlock.style.zIndex = 1000;
             document.body.append(thisBlock);
 
@@ -81,7 +80,7 @@ for (var i = 0; i < h; i++) {
                 new_x = pageX - shiftX;
                 new_y = pageY - shiftY;
                 isBlockMoved = [];
-                moveOtherBlock(thisBlock, 0, 0);
+                moveBlock(thisBlock, 0, 0);
             }
 
             function onMouseMove(event) {
@@ -110,8 +109,7 @@ for (var i = 0; i < h; i++) {
 
 for (var i = 0; i < h; i++) {
     for (var k = 0; k < w; k++) {
-        ;
-        initial_info(block[i][k].id, i, k);
+        initial_info(block[i][k].id, i, k); // информация о всех блоках
     }
 }
 
@@ -120,68 +118,70 @@ for (var i = 0; i < h; i++) {
 function check_hook(el) {
     if (!arr_checked[el.id]) {
 
-        var block_on_mouse_top = parseInt(el.style.top, 10);
-        var block_on_mouse_left = parseInt(el.style.left, 10);
-        var block_2;
+        var el_on_mouse_top = parseInt(el.style.top, 10);
+        var el_on_mouse_left = parseInt(el.style.left, 10);
+        var adjacent_el;
         el.style.zIndex = 0;
         isBlockMoved = [];
         arr_checked_moved = [];
         if (info_about_el_in_block[el.id].left && !info_about_el_in_block[el.id].left.isHooked) {  //присоединить справа
-            block_2 = info_about_el_in_block[el.id].left.block;
-            block_top = parseInt(block_2.style.top, 10);
-            block_left = parseInt(block_2.style.left, 10);
+            adjacent_el = info_about_el_in_block[el.id].left.block;
+            block_top = parseInt(adjacent_el.style.top, 10);
+            block_left = parseInt(adjacent_el.style.left, 10);
 
+            if (((block_top - 10 < el_on_mouse_top) && (block_top + 10 > el_on_mouse_top)) &&
+                ((block_left - 10 + w_el < el_on_mouse_left) && (block_left + 10 + w_el > el_on_mouse_left))) {
 
-            if (((block_top - 10 < block_on_mouse_top) && (block_top + 10 > block_on_mouse_top)) &&
-                ((block_left - 10 + w_block < block_on_mouse_left) && (block_left + 10 + w_block > block_on_mouse_left))) {
-
-                moveAllBlock(el, block_top - parseInt(el.style.top, 10), block_left + w_block - parseInt(el.style.left, 10))
+                connectBlock(el, block_top - parseInt(el.style.top, 10), block_left + w_el - parseInt(el.style.left, 10))
 
                 info_about_el_in_block[el.id].left.isHooked = true;
-                info_about_el_in_block[block_2.id].right.isHooked = true;
+                info_about_el_in_block[adjacent_el.id].right.isHooked = true;
                 return;
             }
         }
         if (info_about_el_in_block[el.id].right && !info_about_el_in_block[el.id].right.isHooked) { //присоединить слева
-            block_2 = info_about_el_in_block[el.id].right.block;
-            block_top = parseInt(block_2.style.top, 10);
-            block_left = parseInt(block_2.style.left, 10);
-            if (((block_top - 10 < block_on_mouse_top) && (block_top + 10 > block_on_mouse_top)) &&
-                ((block_left - 10 - w_block < block_on_mouse_left) && (block_left + 10 - w_block > block_on_mouse_left))) {
+            adjacent_el = info_about_el_in_block[el.id].right.block;
+            block_top = parseInt(adjacent_el.style.top, 10);
+            block_left = parseInt(adjacent_el.style.left, 10);
 
-                moveAllBlock(el, block_top - parseInt(el.style.top, 10), block_left - w_block - parseInt(el.style.left, 10))
+            if (((block_top - 10 < el_on_mouse_top) && (block_top + 10 > el_on_mouse_top)) &&
+                ((block_left - 10 - w_el < el_on_mouse_left) && (block_left + 10 - w_el > el_on_mouse_left))) {
+
+                connectBlock(el, block_top - parseInt(el.style.top, 10), block_left - w_el - parseInt(el.style.left, 10))
 
                 info_about_el_in_block[el.id].right.isHooked = true;
-                info_about_el_in_block[block_2.id].left.isHooked = true;
+                info_about_el_in_block[adjacent_el.id].left.isHooked = true;
                 return;
             }
         }
 
         if (info_about_el_in_block[el.id].top && !info_about_el_in_block[el.id].top.isHooked) { // присоеднить сверху
-            block_2 = info_about_el_in_block[el.id].top.block;
-            block_top = parseInt(block_2.style.top, 10);
-            block_left = parseInt(block_2.style.left, 10);
-            if (((block_top - 10 + h_block < block_on_mouse_top) && (block_top + 10 + h_block > block_on_mouse_top)) &&
-                ((block_left - 10 < block_on_mouse_left) && (block_left + 10 > block_on_mouse_left))) {
+            adjacent_el = info_about_el_in_block[el.id].top.block;
+            block_top = parseInt(adjacent_el.style.top, 10);
+            block_left = parseInt(adjacent_el.style.left, 10);
 
-                moveAllBlock(el, block_top + h_block - parseInt(el.style.top, 10), block_left - parseInt(el.style.left, 10))
+            if (((block_top - 10 + h_el < el_on_mouse_top) && (block_top + 10 + h_el > el_on_mouse_top)) &&
+                ((block_left - 10 < el_on_mouse_left) && (block_left + 10 > el_on_mouse_left))) {
+
+                connectBlock(el, block_top + h_el - parseInt(el.style.top, 10), block_left - parseInt(el.style.left, 10))
 
                 info_about_el_in_block[el.id].top.isHooked = true;
-                info_about_el_in_block[block_2.id].bottom.isHooked = true;
+                info_about_el_in_block[adjacent_el.id].bottom.isHooked = true;
                 return;
             }
         }
         if (info_about_el_in_block[el.id].bottom && !info_about_el_in_block[el.id].bottom.isHooked) { //присоеднить снизу
-            block_2 = info_about_el_in_block[el.id].bottom.block;
-            block_top = parseInt(block_2.style.top, 10);
-            block_left = parseInt(block_2.style.left, 10);
-            if (((block_top - 10 - h_block < block_on_mouse_top) && (block_top + 10 - h_block > block_on_mouse_top)) &&
-                ((block_left - 10 < block_on_mouse_left) && (block_left + 10 > block_on_mouse_left))) {
+            adjacent_el = info_about_el_in_block[el.id].bottom.block;
+            block_top = parseInt(adjacent_el.style.top, 10);
+            block_left = parseInt(adjacent_el.style.left, 10);
+            
+            if (((block_top - 10 - h_el < el_on_mouse_top) && (block_top + 10 - h_el > el_on_mouse_top)) &&
+                ((block_left - 10 < el_on_mouse_left) && (block_left + 10 > el_on_mouse_left))) {
 
-                moveAllBlock(el, block_top - h_block - parseInt(el.style.top, 10), block_left - parseInt(el.style.left, 10))
+                connectBlock(el, block_top - h_el - parseInt(el.style.top, 10), block_left - parseInt(el.style.left, 10))
 
                 info_about_el_in_block[el.id].bottom.isHooked = true;
-                info_about_el_in_block[block_2.id].top.isHooked = true;
+                info_about_el_in_block[adjacent_el.id].top.isHooked = true;
                 return;
             }
         }
@@ -201,7 +201,7 @@ function check_hook(el) {
     }
 }
 
-function moveOtherBlock(movedBlock, y, x) {
+function moveBlock(movedBlock, y, x) {
     if (!isBlockMoved[movedBlock.id]) {
         isBlockMoved[movedBlock.id] = true;
         movedBlock.style.top = new_y + y + "px";
@@ -209,21 +209,21 @@ function moveOtherBlock(movedBlock, y, x) {
         movedBlock.style.zIndex = 1;
 
         if (info_about_el_in_block[movedBlock.id].left && info_about_el_in_block[movedBlock.id].left.isHooked) {
-            moveOtherBlock(info_about_el_in_block[movedBlock.id].left.block, y, -w_block + x)
+            moveBlock(info_about_el_in_block[movedBlock.id].left.block, y, -w_el + x)
         }
         if (info_about_el_in_block[movedBlock.id].top && info_about_el_in_block[movedBlock.id].top.isHooked) {
-            moveOtherBlock(info_about_el_in_block[movedBlock.id].top.block, -h_block + y, x)
+            moveBlock(info_about_el_in_block[movedBlock.id].top.block, -h_el + y, x)
         }
         if (info_about_el_in_block[movedBlock.id].right && info_about_el_in_block[movedBlock.id].right.isHooked) {
-            moveOtherBlock(info_about_el_in_block[movedBlock.id].right.block, y, w_block + x)
+            moveBlock(info_about_el_in_block[movedBlock.id].right.block, y, w_el + x)
         }
         if (info_about_el_in_block[movedBlock.id].bottom && info_about_el_in_block[movedBlock.id].bottom.isHooked) {
-            moveOtherBlock(info_about_el_in_block[movedBlock.id].bottom.block, h_block + y, x)
+            moveBlock(info_about_el_in_block[movedBlock.id].bottom.block, h_el + y, x)
         }
     }
 }
 
-function moveAllBlock(movedEl, onTop, onLeft) {
+function connectBlock(movedEl, onTop, onLeft) {
 
     if (!arr_checked_moved[movedEl.id]) {
         movedEl.style.left = parseInt(movedEl.style.left, 10) + onLeft + "px";
@@ -231,16 +231,16 @@ function moveAllBlock(movedEl, onTop, onLeft) {
         arr_checked_moved[movedEl.id] = true;
 
         if (info_about_el_in_block[movedEl.id].left && info_about_el_in_block[movedEl.id].left.isHooked) {
-            moveAllBlock(info_about_el_in_block[movedEl.id].left.block, onTop, onLeft)
+            connectBlock(info_about_el_in_block[movedEl.id].left.block, onTop, onLeft)
         }
         if (info_about_el_in_block[movedEl.id].right && info_about_el_in_block[movedEl.id].right.isHooked) {
-            moveAllBlock(info_about_el_in_block[movedEl.id].right.block, onTop, onLeft)
+            connectBlock(info_about_el_in_block[movedEl.id].right.block, onTop, onLeft)
         }
         if (info_about_el_in_block[movedEl.id].top && info_about_el_in_block[movedEl.id].top.isHooked) {
-            moveAllBlock(info_about_el_in_block[movedEl.id].top.block, onTop, onLeft)
+            connectBlock(info_about_el_in_block[movedEl.id].top.block, onTop, onLeft)
         }
         if (info_about_el_in_block[movedEl.id].bottom && info_about_el_in_block[movedEl.id].bottom.isHooked) {
-            moveAllBlock(info_about_el_in_block[movedEl.id].bottom.block, onTop, onLeft)
+            connectBlock(info_about_el_in_block[movedEl.id].bottom.block, onTop, onLeft)
         }
 
     }
